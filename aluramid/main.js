d1c2aba0ac621534ee68e505a05fbfc0f1605733
RoMonastero <romonastero@gmail.com>
2022-07-06 17:28:25 -0300

    finalizando primeiro curso de JavaScript da Fromação

--- aluramid/main.js
+++ aluramid/main.js
@@ -1,5 +1,31 @@
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play()
+function tocaSom(som) {
+   const elemento = document.querySelector(`#som_${som}`)
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play()
+    }else {
+        console.log('Elemento não encontrado ou seletor inválido')
+    }
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom
+const listaDeTeclas = document.querySelectorAll('.tecla')
+
+for (let count = 0; count < listaDeTeclas.length; count++) {
+    const tecla = listaDeTeclas[count]
+
+    const instrumento = tecla.classList[1]
+    
+    tecla.onclick = function (){
+        tocaSom(instrumento)
+    }
+
+    tecla.onkeydown = function (evento) {
+        if(evento.code === "Enter" || evento.code === "Space") {
+            tecla.classList.add('ativa')
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa')
+    }
+    
+}
--- ex01/main.js
+++ ex01/main.js
@@ -1,1 +1,29 @@
-document.querySelector('input[type="tel"]') //Dessa forma o input do type tal é selecionado
+const tel = document.querySelector('input[type="tel"]') //Dessa forma o input do type tal é selecionado
+
+function addFoneNumber(number) {
+    tel.value += number
+}
+
+const listaDeBotoes = document.querySelectorAll("input[type='button']")
+
+
+for (let count = 0; count < listaDeBotoes.length; count++) {
+    const botao = listaDeBotoes[count];
+    
+    botao.onclick = function () {
+        addFoneNumber(botao.value)
+    }
+
+    botao.onkeydown = function (evento) {
+        if(evento.code === "Enter" || evento.code === "Space") {
+            botao.classList.add('ativa')
+        }
+    }
+
+    botao.onkeyup = function () {
+        botao.classList.remove('ativa')
+    }
+    
+    
+}
+console.log(listaDeBotoes)
